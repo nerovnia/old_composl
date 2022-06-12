2a5687ef4ba78432265abbe8f8583a0e9a3853dd
Volodymyr Nerovnia <nerv@i.ua>
2022-06-12 14:45:55 +0300

    Init express

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="hello">
+  <div id="homePage" class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
+    <h2>Введення, коригуання показів та проплат</h2>
+    <form action="">
+      <div id="year">
+        <label for="">Рік</label>
+        <select name="" id="">
+          <option value="">2020</option>
+          <option value="">2021</option>
+          <option value="">2022</option>
+        </select>
+      </div>
+      <input type="button" value="Далі >">
+    </form>
+    <!--
     <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
@@ -27,6 +30,20 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    -->
+  </div>
+  <div id="oblPosl">
+    <div id="navComp">
+      <nav>
+        <h4>Компанії:</h4>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+    <div id="listOrders">
+      
+    </div>
   </div>
 </template>
 
